006c527ee0ba5ddfde5686fb844ad3c7162ec856
FABRICAS <armiescalanteli@ittepic.edu.mx>
2018-02-12 08:48:34 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/tam_laboratorio_115_vicentemz/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.tam_laboratorio_115_vicentemz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
